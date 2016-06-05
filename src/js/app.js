33ee56fc282ad8f4bab0780692338f690e6a143f
fahrstuhl <fahrstuhl@mailbox.tu-berlin.de>
2016-06-05 15:39:08 +0200

    changes website to gh-pages

--- src/js/app.js
+++ src/js/app.js
@@ -32,7 +32,7 @@ Pebble.addEventListener('ready', function() {
 });
 
 Pebble.addEventListener('showConfiguration', function() {
-    var url = 'https://what.re/pebble/config/index.html';
+    var url = 'https://fahrstuhl.github.io/ANote/config/index.html';
     console.log('Showing configuration page: ' + url);
 
     Pebble.openURL(url);
