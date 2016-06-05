437ef421f66f06536a0805997f4de51bd76c9d3d
fahrstuhl <gabriel.d.zoeller@campus.tu-berlin.de>
2016-06-05 04:23:57 +0200

    adds support for localStorage

--- src/js/app.js
+++ src/js/app.js
@@ -1,36 +1,69 @@
 Pebble.addEventListener('ready', function() {
-  console.log('PebbleKit JS ready!');
+    console.log('PebbleKit JS ready!');
+    var note;
+    var font_size;
+    if(localStorage['note'] && localStorage['font_size'] ) {
+        note = localStorage.getItem('note');
+        font_size = localStorage.getItem('font_size');
+    }
+    else {
+        note = "Please set your note in the settings";
+        font_size = 24;
+    }
+    var dict = {};
+
+    var buffer = [];
+    var utf8 = unescape(encodeURIComponent(note));
+    for (var i = 0; i < utf8.length; i++) {
+        buffer.push(utf8.charCodeAt(i));
+    }
+    buffer.push(0);
+
+    dict['AppKeyNote'] = buffer;
+    dict['AppKeyNoteLength'] = buffer.length;
+    dict['AppKeyFontSize'] = parseInt(font_size);
+    console.log('Sending: ' + JSON.stringify(dict))
+    // Send to watchapp
+    Pebble.sendAppMessage(dict, function() {
+        console.log('Send successful: ' + JSON.stringify(dict));
+    }, function() {
+        console.log('Send failed!');
+    });
 });
 
 Pebble.addEventListener('showConfiguration', function() {
-  var url = 'https://what.re/pebble/config/index.html';
-  console.log('Showing configuration page: ' + url);
+    var url = 'https://what.re/pebble/config/index.html';
+    console.log('Showing configuration page: ' + url);
 
-  Pebble.openURL(url);
+    Pebble.openURL(url);
 });
 
 Pebble.addEventListener('webviewclosed', function(e) {
-  var configData = JSON.parse(decodeURIComponent(e.response));
-  console.log('Configuration page returned: ' + JSON.stringify(configData));
+    var configData = JSON.parse(decodeURIComponent(e.response));
+    console.log('Configuration page returned: ' + JSON.stringify(configData));
 
-  var note = configData['note_input']
+    var note = configData['note_input']
+    localStorage.setItem('note', note);
+    var font_size = configData['font_size'];
+    localStorage.setItem('font_size', font_size);
 
-  var dict = {};
+    var dict = {};
 
-  var buffer = [];
-  var utf8 = unescape(encodeURIComponent(note));
-  for (var i = 0; i < utf8.length; i++) {
-      buffer.push(utf8.charCodeAt(i));
-  }
-  buffer.push(0);
+    var buffer = [];
+    var utf8 = unescape(encodeURIComponent(note));
+    for (var i = 0; i < utf8.length; i++) {
+        buffer.push(utf8.charCodeAt(i));
+    }
+    buffer.push(0);
 
-  dict['AppKeyNote'] = note;
-  dict['AppKeyNoteLength'] = note.length;
-  console.log('Sending: ' + JSON.stringify(dict))
-  // Send to watchapp
-  Pebble.sendAppMessage(dict, function() {
-    console.log('Send successful: ' + JSON.stringify(dict));
-  }, function() {
-    console.log('Send failed!');
-  });
+    dict['AppKeyNote'] = buffer;
+    dict['AppKeyNoteLength'] = buffer.length;
+    dict['AppKeyFontSize'] = parseInt(font_size);
+    console.log('Sending: ' + JSON.stringify(dict))
+    // Send to watchapp
+    Pebble.sendAppMessage(dict, function() {
+        console.log('Send successful: ' + JSON.stringify(dict));
+    }, function() {
+        console.log('Send failed!');
+    });
 });
